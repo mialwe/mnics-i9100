70d4113127d0f2cf1e176a7cc07613d7932d575e
Michael Weingaertner <mialwe@HAL.(none)>
2013-01-09 22:25:19 +0100

    initramfs: simplify post-init.sh, adjust scheduler values

--- initramfs/sbin/post-init.sh
+++ initramfs/sbin/post-init.sh
@@ -10,116 +10,72 @@ exec 2>&1
 $BB mount -t rootfs -o remount,rw rootfs
 
 # start logfile output
-echo "MIDNIGHT-I9100 POST-INIT BOOT LOG"
+echo "MNICS-I9100 POST-INIT BOOT LOG"
 echo "$(date)"
+
 # log basic system information
 echo -n "Kernel: ";$BB uname -r
 echo -n "PATH: ";echo $PATH
 echo -n "ROM: ";cat /system/build.prop|$BB grep ro.build.display.id
 echo -n "BusyBox:";$BB|$BB grep BusyBox
 
-echo "$(date) modules"
+echo "$(date) /lib/modules:"
 ls -l /lib/modules
 
-echo "$(date) modules loaded"
+echo "$(date) modules loaded:"
 $BB lsmod
 
-# print file contents <string messagetext><file output>
-cat_msg_sysfile() {
-    MSG=$1
-    SYSFILE=$2
-    echo -n "$(date) $MSG"
-    cat $SYSFILE
-}
-
-# partitions
-echo "$(date) mount"
+echo "$(date) setting general mount options..."
 for i in $($BB mount | $BB grep relatime | $BB cut -d " " -f3);do
     $BB mount -o remount,noatime $i
 done
+
+echo "$(date) setting EXT4 mount options..."
 for i in $($BB mount | $BB grep ext4 | $BB cut -d " " -f3);do
     $BB mount -o remount,commit=20 $i
 done
 
-$BB mount
-
-# read_ahead
-echo "$(date) read_ahead"
+echo "$(date) setting read_ahead values"
 echo 128 > /sys/block/mmcblk0/bdi/read_ahead_kb
 echo 128 > /sys/block/mmcblk1/bdi/read_ahead_kb
 
 # logger compiled into the kernel for now...
 # insmod /lib/modules/logger.ko
 
-# vm tweaks
-echo "$(date) vm"
-echo "0" > /proc/sys/vm/swappiness # Not really needed as no /swap used...
-echo "1500" > /proc/sys/vm/dirty_writeback_centisecs # Flush after 20sec. (o:500)
-echo "1500" > /proc/sys/vm/dirty_expire_centisecs # Pages expire after 20sec. (o:200)
-echo "10" > /proc/sys/vm/dirty_background_ratio # flush pages later (default 5% active mem)
-echo "20" > /proc/sys/vm/dirty_ratio # process writes pages later (default 20%)
-echo "3" > /proc/sys/vm/page-cluster
-echo "0" > /proc/sys/vm/laptop_mode
-echo "0" > /proc/sys/vm/oom_kill_allocating_task
-echo "0" > /proc/sys/vm/panic_on_oom
-echo "1" > /proc/sys/vm/overcommit_memory
-cat_msg_sysfile "swappiness: " /proc/sys/vm/swappiness
-cat_msg_sysfile "dirty_writeback_centisecs: " /proc/sys/vm/dirty_writeback_centisecs
-cat_msg_sysfile "dirty_expire_centisecs: " /proc/sys/vm/dirty_expire_centisecs
-cat_msg_sysfile "dirty_background_ratio: " /proc/sys/vm/dirty_background_ratio
-cat_msg_sysfile "dirty_ratio: " /proc/sys/vm/dirty_ratio
-cat_msg_sysfile "page-cluster: " /proc/sys/vm/page-cluster
-cat_msg_sysfile "laptop_mode: " /proc/sys/vm/laptop_mode
-cat_msg_sysfile "oom_kill_allocating_task: " /proc/sys/vm/oom_kill_allocating_task
-cat_msg_sysfile "panic_on_oom: " /proc/sys/vm/panic_on_oom
-cat_msg_sysfile "overcommit_memory: " /proc/sys/vm/overcommit_memory
+echo "$(date) setting vm tweaks..."
+echo "0" > /proc/sys/vm/swappiness                   # Not really needed as no /swap used...
+echo "1500" > /proc/sys/vm/dirty_writeback_centisecs # Flush after 15 sec. (o:500)
+echo "1500" > /proc/sys/vm/dirty_expire_centisecs    # Pages expire after 15 sec. (o:200)
+echo "10" > /proc/sys/vm/dirty_background_ratio      # flush pages later (default 5% active mem)
+echo "20" > /proc/sys/vm/dirty_ratio
 
-# security enhancements
 # rp_filter must be reset to 0 if TUN module is used (issues)
-echo "$(date) IPV4 sec"
+echo "$(date) setting IPV4 sec tweaks..."
 echo 0 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
 echo 2 > /proc/sys/net/ipv6/conf/all/use_tempaddr
 echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route
 echo 0 > /proc/sys/net/ipv4/conf/all/send_redirects
 echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
-echo -n "$(date) SEC: ip_forward :";cat /proc/sys/net/ipv4/ip_forward
-echo -n "$(date) SEC: rp_filter :";cat /proc/sys/net/ipv4/conf/all/rp_filter
-echo -n "$(date) SEC: use_tempaddr :";cat /proc/sys/net/ipv6/conf/all/use_tempaddr
-echo -n "$(date) SEC: accept_source_route :";cat /proc/sys/net/ipv4/conf/all/accept_source_route
-echo -n "$(date) SEC: send_redirects :";cat /proc/sys/net/ipv4/conf/all/send_redirects
-echo -n "$(date) SEC: icmp_echo_ignore_broadcasts :";cat /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
 
-# setprop tweaks
-echo "$(date) prop"
+echo "$(date) setting setprop tweaks..."
 setprop wifi.supplicant_scan_interval 180
-echo -n "wifi.supplicant_scan_interval (is this actually used?): ";getprop wifi.supplicant_scan_interval
 
-# kernel tweaks
-echo "$(date) kernel"
-echo 3750000 > /proc/sys/kernel/sched_latency_ns
-echo 750000 > /proc/sys/kernel/sched_wakeup_granularity_ns
-echo 750000 > /proc/sys/kernel/sched_min_granularity_ns
-echo 0 > /proc/sys/kernel/panic_on_oops
-echo 0 > /proc/sys/kernel/panic
-cat_msg_sysfile "sched_latency_ns: " /proc/sys/kernel/sched_latency_ns
-cat_msg_sysfile "sched_wakeup_granularity_ns: " /proc/sys/kernel/sched_wakeup_granularity_ns
-cat_msg_sysfile "sched_min_granularity_ns: " /proc/sys/kernel/sched_min_granularity_ns
-cat_msg_sysfile "panic_on_oops: " /proc/sys/kernel/panic_on_oops
-cat_msg_sysfile "panic: " /proc/sys/kernel/panic
+echo "$(date) setting scheduler/kernel tweaks..."
+echo 2000000 > /proc/sys/kernel/sched_latency_ns
+echo 1000000 > /proc/sys/kernel/sched_wakeup_granularity_ns
+echo 1000000 > /proc/sys/kernel/sched_min_granularity_ns
 
-# IO scheduler tweaks
-echo "$(date) IO"
+echo "$(date) setting IO tweaks..."
 for i in $($BB find /sys/block/mmc*);do
-    if [ -d "$i/queue" ];then
-        echo "$(date) adjusting $i" 
+    if [ -d "$i/queue" ];then 
         echo "0" > $i/queue/rotational
         echo "0" > $i/queue/iostats
     fi	
 done
 
 # disable some debugging, by Speedmod/HC
-echo "$(date) debugging"
+echo "$(date) setting debug masks..."
 echo "0" > /sys/module/wakelock/parameters/debug_mask
 echo "0" > /sys/module/userwakelock/parameters/debug_mask
 echo "0" > /sys/module/earlysuspend/parameters/debug_mask
@@ -174,6 +130,7 @@ else
         done
     fi
 fi
+
 # the end
 $BB mount -t rootfs -o remount,ro rootfs
 exit 0
